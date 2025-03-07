8c4ccf2e3ba84bbfa3256113f7cb3ae0a4324c03
CodewithRohitRajput <rohitsinghrajput2k24@gmail.com>
2025-03-07 23:28:22 +0530

    fixed space

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 
         <div className=''>
             <form action="" onSubmit={makeNote}>
-                <textarea type="text" placeholder='Whats on your Mind ? ' className=' mt-10 border-4 border-blue-800 rounded-lg w-96 h-56 px-2 py-2 font-sans'  value={note} onChange={(e)=> setNotes(e.target.value)} />
+                <textarea type="text" placeholder='Whats on your Mind ? ' className=' mt-10 border-4 border-blue-800 rounded-lg w-96 h-56 px-4 py-2 font-sans'  value={note} onChange={(e)=> setNotes(e.target.value)} />
                 <br />
                 <button  type='Submit'  className='px-3 py-4 bg-blue-100 rounded-lg text-blue-600 font-semibold  border-b-4 border-blue-600 ml-36 mt-4 transition-transform active:scale-90 '>
                     Create Note
@@ -34,7 +34,7 @@ const Home = () => {
 <div  
 key={index}
     
-    className='bg-blue-200 text-black from-neutral-400 w-96 h-40 border-b-4 border-blue-600 mt-16 rounded-xl px-2 py-2  '>
+    className='bg-blue-200 text-black from-neutral-400 w-96 h-40 border-b-4 border-blue-600 mt-16 rounded-xl px-5 py-2  '>
                 
                {note}
             </div>
